0671ef689297338dd663b859d381fac0edbf4d61
ehdrjs4502 <ehdrjs4502@naver.com>
2023-06-17 22:54:32 +0900

    0617 store, reducer 기초 배움

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const add = document.getElementById("add");
+const minus = document.getElementById("minus");
+const num = document.getElementById("num");
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+num.innerText = 0;
+
+const reducer = (count = 0) => {
+
+  return count;
+}
+
+const store = createStore(reducer);
+
+console.log(store.getState());
